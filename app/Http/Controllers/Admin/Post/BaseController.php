c04bf36dd575e851fd525e82cef8ddd320fec0b9
Kuleshov Artem <artemkul57@mail.ru>
2023-07-24 10:58:51 +0300

    Создание сервиса для сохр и обновл постов в админке

---
+++ app/Http/Controllers/Admin/Post/BaseController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Post;
+
+use App\Http\Controllers\Controller;
+use App\Service\Admin\PostService;
+
+class BaseController extends Controller
+{
+    protected $service;
+
+    public function __construct(PostService $service)
+    {
+        $this->service = $service;
+    }
+}
---
+++ app/Service/Admin/PostService.php
@@ -0,0 +1,64 @@
+<?php
+
+
+namespace App\Service\Admin;
+
+
+use App\Models\Post;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+
+class PostService
+{
+    public function store($data)
+    {
+        try {
+            DB::beginTransaction();
+
+            $tag_ids = $data['tag_ids'];
+            unset($data['tag_ids']);
+
+            if (isset($data['preview_image'])) {
+                $data['preview_image'] = Storage::disk('public')->put('/images', $data['preview_image']);
+            }
+            if (isset($data['main_image'])) {
+                $data['main_image'] = Storage::disk('public')->put('/images', $data['main_image']);
+            }
+
+            $post = Post::firstOrCreate($data);
+            $post->tags()->attach($tag_ids);
+            DB::commit();
+        } catch (\Exception) {
+            DB::rollBack();
+            abort(500);
+        }
+    }
+
+    public function update($data, $post)
+    {
+        try {
+            DB::beginTransaction();
+
+            $tag_ids = $data['tag_ids'];
+            unset($data['tag_ids']);
+
+            if(isset($data['preview_image'])) {
+                Storage::disk('public')->delete($post->preview_image);
+                $data['preview_image'] = Storage::disk('public')->put('/images', $data['preview_image']);
+            }
+            if(isset($data['main_image'])) {
+                Storage::disk('public')->delete($post->main_image);
+                $data['main_image'] = Storage::disk('public')->put('/images', $data['main_image']);
+            }
+
+            $post->update($data);
+            $post->tags()->sync($tag_ids);
+
+            DB::commit();
+            return $post;
+        } catch (\Exception) {
+            DB::rollBack();
+            abort(500);
+        }
+    }
+}
