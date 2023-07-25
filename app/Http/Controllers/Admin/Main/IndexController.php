762727f9eae02b5163046c8d97a3e8703bd85118
Kuleshov Artem <artemkul57@mail.ru>
2023-07-25 10:27:18 +0300

    Стилизация главной стр админки

--- app/Http/Controllers/Admin/Main/IndexController.php
+++ app/Http/Controllers/Admin/Main/IndexController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers\Admin\Main;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Tag;
+use App\Models\User;
 
 class IndexController extends Controller
 {
     public function __invoke()
     {
-        return view('admin.main.index');
+        $data['users_count'] = User::all()->count();
+        $data['posts_count'] = Post::all()->count();
+        $data['tags_count'] = Tag::all()->count();
+        $data['categories_count'] = Category::all()->count();
+        return view('admin.main.index', compact('data'));
     }
 }
